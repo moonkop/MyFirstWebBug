75eb31cfded215a891f35deb1ea509282a7a8e23
moonkop <moonkop@DESKTOP-O7O378G>
2017-09-08 03:44:39 +0800

    download zhihu img in answer

--- MyFirstWebBug/MyFirstWebBug/MyFirstWebBug.py
+++ MyFirstWebBug/MyFirstWebBug/MyFirstWebBug.py
@@ -1,13 +1,39 @@
+
 import urllib.request
 from bs4 import BeautifulSoup
 
-url = 'http://www.baidu.com/'
-response =urllib.request.urlopen(url)
-str= response.read()
-soup = BeautifulSoup(str,"html.parser")
-print(soup.find('html'))
 
+url = 'https://www.zhihu.com/question/64885015/answer/225276182' 
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'   
+headers = { 'User-Agent' : user_agent }   
+request=urllib.request.Request(url,headers=headers)
 
+response =urllib.request.urlopen(request)
+str1= response.read()
+
+soup = BeautifulSoup(str1,"html.parser")
+list= soup.find_all(attrs={"class":"origin_image"})
+print(len(list))
+i=int(0)
+for img in list:
+    
+    if 'lazy' in img.attrs['class']:
+        continue
+    jpgaddr= img.attrs['data-original']
+    
+    print(str(i)+" =  "+jpgaddr)
+    request=urllib.request.Request(jpgaddr,headers=headers)
+    response =urllib.request.urlopen(request)
+    jpgdata= response.read() 
+    picpath=r"d:\pythondata\bug1\pics\\"
+    wholepath=picpath+str(i)+".jpg"
+    jpgfile= open(wholepath,"wb")
+    jpgfile.write(jpgdata)
+    i+=1
+    
+htmlfile= open("d:\html.html","w")
+print(str,file=htmlfile)
+htmlfile.close()
 
 
 
